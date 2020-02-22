9f93aceda342c73dc562d3f57972b158fab23960
Claire Griffin <clairetg@stanford.edu>
2020-02-21 19:17:39 -0800

    fix broken test, add test for skip=true

--- lockss-tdbxml-maven-plugin/src/test/java/org/lockss/maven/TestTdbXmlMojo.java
+++ lockss-tdbxml-maven-plugin/src/test/java/org/lockss/maven/TestTdbXmlMojo.java
@@ -1,34 +1,59 @@
+/*
+ * Copyright (c) 2020 Board of Trustees of Leland Stanford Jr. University,
+ * all rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of Stanford University shall not
+ * be used in advertising or otherwise to promote the sale, use or other dealings
+ * in this Software without prior written authorization from Stanford University.
+ */
+
 package org.lockss.maven;
 
 import static java.nio.file.FileVisitOption.FOLLOW_LINKS;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugin.logging.SystemStreamLog;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.utils.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.lockss.maven.TdbXmlMojo.TdbFindFile;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import org.lockss.maven.TdbXmlMojo.TdbFindFile;
+
 @RunWith(MockitoJUnitRunner.class)
-public class TdbXmlMojoTest extends AbstractMojoTestCase {
+public class TestTdbXmlMojo extends AbstractMojoTestCase {
 
   File srcDir = getTestFile("target/test-classes/org/lockss/maven/tdb");
   File dstDir = getTestFile("target/test-classes/org/lockss/maven/xml");
@@ -80,7 +105,7 @@ public class TdbXmlMojoTest extends AbstractMojoTestCase {
     File out2 = new File(dstDir, "tdb1.xml");
     inMap.put(in1, out1);
     inMap.put(in2, out2);
-    tdbXmlMojo.setRecurse(false);
+    tdbXmlMojo.setRecurse(true);
     tdbXmlMojo.setClasspath(System.getProperty("java.class.path"));
     tdbXmlMojo.execJavaCmd(inMap);
 
@@ -113,7 +138,7 @@ public class TdbXmlMojoTest extends AbstractMojoTestCase {
 
   @Test
   public void testMojoGoal() throws Exception {
-    File testPom = getTestFile("/target/test-classes/simple-pom.xml");
+    File testPom = getTestFile("src/test/resources/simple-pom.xml");
     assertNotNull(testPom);
     assertTrue(testPom.exists());
     TdbXmlMojo my_mojo = (TdbXmlMojo) lookupMojo("tdbxml", testPom);
